2f453e651268c927ec322af31a88fe59c5f7f0bd
Vadym Shymko <vadymshymko@gmail.com>
2016-10-10 11:19:29 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise",
-        "react",
-        "html"
-    ],
-    "rules": {
-      "no-multi-spaces": "off",
-      "key-spacing": ["error", {
-        "align": {
-          "beforeColon": true,
-          "afterColon": true,
-          "on": "colon"
-        }
-      }],
-      "no-return-assign": "off",
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-    },
-    "env": {
-      "browser": true
-    }
+  "parser": "babel-eslint",
+  "extends": "standard",
+  "plugins": [
+      "standard",
+      "promise",
+      "react",
+      "html"
+  ],
+  "rules": {
+    "no-multi-spaces": "off",
+    "key-spacing": ["error", {
+      "align": {
+        "beforeColon": true,
+        "afterColon": true,
+        "on": "colon"
+      }
+    }],
+    "no-return-assign": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+  },
+  "env": {
+    "browser": true
+  }
 };
