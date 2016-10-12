15d18e507ec3dbe1daaa7a9dcc7be0e4bb2f61cf
Vadym Shymko <vadymshymko@gmail.com>
2016-10-12 20:33:35 +0300

    Added Loaders

---
+++ src/components/loaders/L2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Loader from '../Loader.jsx'
+
+export default class L2 extends React.Component {
+  render () {
+    return (
+      <Loader
+        {...this.props}
+        width  = "fixed"
+        height = "large"
+      />
+    )
+  }
+}
---
+++ src/components/loaders/L3.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Loader from '../Loader.jsx'
+
+export default class L3 extends React.Component {
+  render () {
+    return (
+      <Loader
+        {...this.props}
+        width  = "full"
+        height = "medium"
+      />
+    )
+  }
+}
