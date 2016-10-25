9de2acd8a35801c40938e88e79055e61b655b1fd
kostyac <kostyac@templatemonster.me>
2016-10-25 18:41:56 +0300

    added example dir for debugging QUARK components inside lib itself

---
+++ example/webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    entry : './main.js',
+    output: {
+        path: './',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        hot: true,
+        port: 4444
+    },
+    module: {
+        loaders : [
+            {
+                test: /\.js$/,  //All .js files
+                loaders: ['babel'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.png/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.svg/,
+                loader: "svg-loader"
+            },
+            {
+                test: /\.styl/,
+                loader: "style-loader!css-loader!stylus-loader"
+            },
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
+            },
+        ]
+    }
+}
