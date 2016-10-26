733ad09f1dd86eb46db1c5ecbc63e9d1c068eca5
kostyac <kostyac@templatemonster.me>
2016-10-26 15:19:42 +0300

    added additional utils file + added password field on example page

--- src/components/utils/connectNotificationTrigger.jsx
+++ src/components/utils/connectNotificationTrigger.jsx
@@ -94,7 +94,9 @@ export default function connectNotificationTrigger (Component, props) {
     };
 
     handleClosePopover = (e) => {
-      if (this.props.popover && this.state.notification) {
+      console.log(this.props.popover, this.state.notification);
+      if (this.state.notification) {
+        console.log(1111);
         e.stopPropagation();
 
         if (isMouseOutOfComponent({
