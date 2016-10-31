4b4bdc2de31441f9a244261ca41f85cbddb0b6d8
Vadym Shymko <vadymshymko@gmail.com>
2016-10-31 17:20:14 +0200

    Fix button keyDown event

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -90,13 +90,10 @@ export default class Dropdown extends React.Component {
   handleButtonKeyDown (event) {
     const keyCode = event.keyCode;
 
-    event.stopPropagation();
-
     if (keyCode === 40 && this.props.type !== 3 && this.option0) {
       console.log('keyCode', keyCode);
       console.log('this.props.type', this.props.type);
       console.log('this.option0', this.option0);
-      this.button.blur();
       this.option0.focus();
     }
   }
