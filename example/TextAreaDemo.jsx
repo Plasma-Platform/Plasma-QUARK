a1a59d2bbb681d57805a5c0ad6175bc899ec59b5
ArnieTM <arnie@templatemonster.me>
2016-11-07 17:28:29 +0200

    UPD: TextArea demo page  RDM-79

--- example/TextAreaDemo.jsx
+++ example/TextAreaDemo.jsx
@@ -13,33 +13,39 @@ export default class TextAreaDemo extends React.Component {
         <div className="flex">
           <div className="half-width spacing-right-20">
             <TextAreas.TA1
+              className="spacing-outer-bottom-20"
               label='Text area type = TA1'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA2
+              className="spacing-outer-bottom-20"
               label='Text area type = TA2'
               type = 'search'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA3
+              className="spacing-outer-bottom-20"
               label='Text area type = TA3'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA4
+              className="spacing-outer-bottom-20"
               label='Text area type = TA4'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA5
+              className="spacing-outer-bottom-20"
               label='Text area type = TA5'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
               maxWidth="300"
             />
             <TextAreas.TA6
+              className="spacing-outer-bottom-20"
               label='Text area type = TA6'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
@@ -47,11 +53,13 @@ export default class TextAreaDemo extends React.Component {
               maxLength = {10}
             />
             <TextAreas.TA7
+              className="spacing-outer-bottom-20"
               label='Text area type = TA7'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA8
+              className="spacing-outer-bottom-20"
               label='Text area type = TA8'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
@@ -60,12 +68,14 @@ export default class TextAreaDemo extends React.Component {
           <div className="half-width spacing-left-20">
             <TextAreas.TA1
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA1'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA2
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled = TA2'
               type = 'search'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
@@ -73,36 +83,42 @@ export default class TextAreaDemo extends React.Component {
             />
             <TextAreas.TA3
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA3'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA4
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA4'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA5
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA5'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA6
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA6'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA7
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA7'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
             />
             <TextAreas.TA8
               disabled
+              className="spacing-outer-bottom-20"
               label='Disabled TA8'
               notificationText='dfkhnvkjdfnjk djkfdfjkndfkjgndfkj dfjgkndfkjgndfkjgndfjkg dfgkjndfkjgndf gjk'
               notificationType='N2F'
