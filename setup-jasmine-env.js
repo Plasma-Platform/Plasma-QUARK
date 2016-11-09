470a4d71365d332f56d2a23c227ce3305150584b
ch <ch@devoffice.ninja>
2016-11-09 15:28:20 +0200

    Added junit test results for CI.

---
+++ setup-jasmine-env.js
@@ -0,0 +1,7 @@
+/*globals jasmine*/
+
+var jasmineReporters = require('jasmine-reporters');
+var junitReporter = new jasmineReporters.JUnitXmlReporter({
+  savePath: 'output/',
+});
+jasmine.getEnv().addReporter(junitReporter);
