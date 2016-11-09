ca9c2e4dfc26b25c7bcf1001ccdc2211d836cd66
ArnieTM <arnie@templatemonster.me>
2016-11-09 10:42:47 +0200

    UPD: updated DEMO page

--- example/App.jsx
+++ example/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import 'ui-toolkit/css/main.css';
 import '../css/main.css';
 import './main.less';
@@ -7,7 +8,17 @@ export default class App extends React.Component {
   render () {
     return (
       <article>
-        <h1 className="spacing-outer-bottom-20">Demo app</h1>
+        <div className="flex align-center">
+          <h1 className="spacing-outer-bottom-20">QUARK Components Demo App</h1>
+          <ul className="flex align-center demo-menu">
+            <li className="spacing-right-20">
+              <Link to="/textfield">Textfield</Link>
+            </li>
+            <li className="spacing-right-20">
+              <Link to="/textarea">Textarea</Link>
+            </li>
+          </ul>
+        </div>
         {this.props.children}
       </article>
     );
--- example/main.js
+++ example/main.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './App.jsx';
 import TextAreaDemo from './TextAreaDemo.jsx';
 import TextFieldDemo from './TextFieldDemo.jsx';
 
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <Route path="ta" component={TextAreaDemo}/>
-      <Route path="tf" component={TextFieldDemo}/>
+      <Route path="textarea" component={TextAreaDemo}/>
+      <Route path="textfield" component={TextFieldDemo}/>
     </Route>
-  </Router>), document.getElementById('app'));
+  </Router>
+), document.getElementById('app'));
