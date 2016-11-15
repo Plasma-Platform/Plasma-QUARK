e66f14225633028964a882c8a89d4f64653cb8ef
kostyac <kostyac@templatemonster.me>
2016-11-15 17:11:06 +0200

    removed unnecessary content from debugging page

--- example/main.js
+++ example/main.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TextFields from '../lib/textFields';
-import Typography from '../lib/typography';
-import Notifications from '../lib/notifications';
 
 import '../css/main.css';
 
@@ -42,40 +40,6 @@ export default class Element extends React.Component {
           value=''
         />
 
-        <Notifications.N1J
-          className='facebook-notification'
-          text='sdfsdf'
-          width='100%'
-          typographyCode='T3'
-        />
-
-        <Typography.H0 >Text<a href="#">111</a><a href="#" className="error-link">111</a><a href="#" className="success-link">111</a></Typography.H0>
-        <Typography.H1 >Text</Typography.H1>
-        <Typography.H2 >Text</Typography.H2>
-        <Typography.H3 >Text</Typography.H3><br/>
-
-        <Typography.T1 type="default">Text <a href="#">111</a></Typography.T1><br/>
-        <Typography.T4 type="secondary">Text <a href="#">111</a></Typography.T4><br/>
-        <Typography.T2 type="error">Text <a href="#">111</a></Typography.T2><br/>
-        <Typography.T3 type="success">Text <a href="#">111</a></Typography.T3><br/>
-
-        <Typography.T5 type="default">Text</Typography.T5><br/>
-        <Typography.T6 type="default">Text</Typography.T6>
-
-        <div style={{backgroundColor: '#000000'}}>
-          <Typography.H0 themeType="dark">Text<a href="#">111</a><a href="#" className="error-link">111</a><a href="#" className="success-link">111</a></Typography.H0>
-          <Typography.H1 themeType="dark">Text</Typography.H1>
-          <Typography.H2 themeType="dark">Text</Typography.H2>
-          <Typography.H3 themeType="dark">Text</Typography.H3><br/>
-
-          <Typography.T1 type="default" themeType="dark">Text <a href="#">111</a></Typography.T1><br/>
-          <Typography.T4 type="secondary" themeType="dark">Text <a href="#">111</a></Typography.T4><br/>
-          <Typography.T2 type="error" themeType="dark">Text <a href="#">111</a></Typography.T2><br/>
-          <Typography.T3 type="success" themeType="dark">Text <a href="#">111</a></Typography.T3><br/>
-
-          <Typography.T5 type="default" themeType="dark">Text</Typography.T5><br/>
-          <Typography.T6 type="default" themeType="dark">Text</Typography.T6>
-        </div>
       </div>
     );
   }
