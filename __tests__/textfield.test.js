60caccadfaa6e8a2e780572d9f8e4aa3dc77bee9
ArnieTM <arnie@templatemonster.me>
2016-11-16 10:34:36 +0200

    ADD: added tests

--- __tests__/textfield.test.js
+++ __tests__/textfield.test.js
@@ -3,14 +3,15 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 import TextFields from '../src/components/textFields';
+import ControlledComponent from '../src/components/AbstractField.jsx';
 
-describe('Testing Decorators [F1, F2, F3, F4]', () => {
+describe('Testing TextField high level components [F1, F2, F3, F4]', () => {
   let PROPS = {
     sizeType : ['F1', 'F2', 'F3', 'F4'],
     type     : ['email', 'password', 'text', 'search']
   };
 
-  it('4 sizes of TF with default parameters', () => {
+  it('All 4 sizes of TF with default parameters (snapshots)', () => {
     PROPS.sizeType.forEach((size) => {
       let TextField = TextFields[size];
       let node = shallow(<TextField />).html();
@@ -18,7 +19,7 @@ describe('Testing Decorators [F1, F2, F3, F4]', () => {
     });
   });
 
-  it('4 sizes of TF * 4 input types ', () => {
+  it('All 4 sizes of TF * each 4 input types (snapshots)', () => {
     PROPS.sizeType.forEach((size) => {
       let TextField = TextFields[size];
       PROPS.type.forEach((type) => {
@@ -28,7 +29,7 @@ describe('Testing Decorators [F1, F2, F3, F4]', () => {
     });
   });
 
-  it('TextField[Type].value must return a value of input', () => {
+  it('textField.value must return a value of input', () => {
     PROPS.sizeType.forEach((size) => {
       let TextField = TextFields[size];
       let node = mount(<TextField value='test value' />);
@@ -36,4 +37,30 @@ describe('Testing Decorators [F1, F2, F3, F4]', () => {
       expect(value).toEqual('test value');
     });
   });
+
+  it('componentType of TextField must be "textfield"', () => {
+    let node = mount(<TextFields.F1 />);
+    let type = node.find('TextField').first().props().componentType;
+    expect(type).toEqual('textfield');
+  });
+
+  it('sizeType prop points to right size', () => {
+    PROPS.sizeType.forEach((size) => {
+      let TextField = TextFields[size];
+      let node = mount(<TextField />);
+      let sizeType = node.find('TextField').first().props().sizeType;
+      expect(sizeType).toEqual(size);
+    });
+  });
+
+  it('type of input must be "text" by default', () => {
+    let node = mount(<TextFields.F1 />);
+    let type = node.find('TextField').first().props().type;
+    expect(type).toEqual('text');
+  });
+});
+
+describe('Controlled component (AbstractField) testing', () => {
+
+
 });
