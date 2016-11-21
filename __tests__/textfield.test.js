b2329ec00688942be84fcfb1582f01cb80bddd05
ArnieTM <arnie@templatemonster.me>
2016-11-21 14:15:14 +0200

    FIX: textField tests

--- __tests__/textfield.test.js
+++ __tests__/textfield.test.js
@@ -1,114 +1,115 @@
 /* global expect:false, test:false, it:false, describe:false, jest:false, beforeEach:false */
 
 import React from 'react';
-import {shallow, mount, render} from 'enzyme';
-import TextFields from '../src/components/textFields';
+import {shallow, mount} from 'enzyme';
+import TextField from '../src/components/textFields';
+import TextFieldPresent from '../src/components/TextField.jsx';
 import AbstractField from '../src/components/AbstractField.jsx';
 
 describe('1 TextField high level components [F1, F2, F3, F4] testing', () => {
   let PROPS = {
-    sizeType : ['F1', 'F2', 'F3', 'F4'],
-    type     : ['email', 'password', 'text', 'search']
+    type: ['email', 'password', 'text', 'search']
   };
 
-  let testNode;
-  let input;
-  let focus;
-  let blur;
-  let change;
+  let callback;
+  let tfCount;
+  let textFields = [];
 
   beforeEach(() => {
-    focus = jest.fn();
-    blur = jest.fn();
-    change = jest.fn();
-    testNode = mount(<TextFields.F1 onFocus={focus} onBlur={blur} onChange={change}/>);
-    input = testNode.find('input').first();
+    callback = jest.fn();
+    tfCount = Object.keys(TextField).length;
+    for (let i = 1; i <= tfCount; i++) {
+      let componentType = `F${i}`;
+      let component = TextField[componentType];
+      textFields.push(component);
+    }
   });
 
   it('1.1 makes snapshots for All 4 sizes of TF with default parameters', () => {
-    PROPS.sizeType.forEach((size) => {
-      let TextField = TextFields[size];
-      let node = shallow(<TextField />).html();
+    textFields.forEach((Component) => {
+      let node = shallow(<Component />).html();
       expect(node).toMatchSnapshot();
     });
   });
 
   it('1.2 makes snapshots for All 4 sizes of TF * each 4 input types', () => {
-    PROPS.sizeType.forEach((size) => {
-      let TextField = TextFields[size];
+    textFields.forEach((Component) => {
       PROPS.type.forEach((type) => {
-        let node = shallow(<TextField type={type} />).html();
+        let node = shallow(<Component type={type} />).html();
         expect(node).toMatchSnapshot();
       });
     });
   });
 
   it('1.3 checks that textField.value returns a value of input', () => {
-    PROPS.sizeType.forEach((size) => {
-      let TextField = TextFields[size];
-      let node = mount(<TextField value='test value' />);
+    textFields.forEach((Component) => {
+      let node = mount(<Component value='test value' />);
       let value = node.component.getInstance().value;
       expect(value).toEqual('test value');
     });
   });
 
   it('1.4 checks that componentType of TextField is "textfield"', () => {
-    let node = mount(<TextFields.F1 />);
-    let type = node.find('TextField').first().props().componentType;
-    expect(type).toEqual('textfield');
+    textFields.forEach((Component) => {
+      let node = mount(<Component />);
+      let type = node.find('TextField').first().props().componentType;
+      expect(type).toEqual('textfield');
+    });
   });
 
   it('1.5 checks that sizeType prop points to right size', () => {
-    PROPS.sizeType.forEach((size) => {
-      let TextField = TextFields[size];
-      let node = mount(<TextField />);
+    for (let i = 1; i <= tfCount; i++) {
+      let size = `F${i}`;
+      let Component = TextField[size];
+      let node = mount(<Component />);
       let sizeType = node.find('TextField').first().props().sizeType;
       expect(sizeType).toEqual(size);
+    }
+  });
+
+  it('1.8 performs focus in field using focus() method', () => {
+    textFields.forEach((Component) => {
+      let node = mount(<Component />);
+      node.node.focus();
+      expect(node.find('TextField').props().focused).toEqual(true);
     });
   });
 
-  it('1.6 checks that type of input is "text" by default', () => {
-    let node = mount(<TextFields.F1 />);
-    let type = node.find('TextField').first().props().type;
-    expect(type).toEqual('text');
-  });
-
-  it('1.7 checks that input type=password has an password reveal icon', () => {
-    let node = mount(<TextFields.F1 type="password"/>);
-    let reveal = node.find('EyePasswordIndicator').first();
-    expect(reveal).toBeDefined();
-  });
-
-  describe('1.2 High level component events testing', () => {
+  describe('1.2 Events', () => {
     it('1.2.1 calls onFocus callback', () => {
-      input.simulate('focus');
-      expect(focus).toBeCalled();
+      textFields.forEach((Component) => {
+        let node = mount(<Component onFocus={callback} />);
+        node.find('input').simulate('focus');
+        expect(callback).toBeCalled();
+      });
     });
 
     it('1.2.2 calls onBlur callback', () => {
-      input.simulate('blur');
-      expect(blur).toBeCalled();
+      textFields.forEach((Component) => {
+        let node = mount(<Component onBlur={callback} />);
+        node.find('input').simulate('blur');
+        expect(callback).toBeCalled();
+      });
     });
 
     it('1.2.3 calls onChange callback', () => {
-      input.simulate('change');
-      expect(change).toBeCalled();
+      textFields.forEach((Component) => {
+        let node = mount(<Component onChange={callback} />);
+        node.find('input').simulate('change');
+        expect(callback).toBeCalled();
+      });
     });
   });
 });
 
-describe('2 Controlled component (AbstractField) testing', () => {
-  let focus;
+describe('2 AbstractField testing', () => {
   let testNode;
   let input;
-  let blur;
-  let change;
+  let callback;
 
   beforeEach(() => {
-    focus = jest.fn();
-    blur = jest.fn();
-    change = jest.fn();
-    testNode = mount(<AbstractField onFocus={focus} onBlur={blur} onChange={change}/>);
+    callback = jest.fn();
+    testNode = mount(<AbstractField onFocus={callback} onBlur={callback} onChange={callback}/>);
     input = testNode.find('input').first();
   });
 
@@ -119,7 +120,7 @@ describe('2 Controlled component (AbstractField) testing', () => {
 
     it('2.1.2 calls onFocus callback', () => {
       input.simulate('focus');
-      expect(focus).toBeCalled();
+      expect(callback).toBeCalled();
     });
 
     it('2.1.3 checks that onFocus added "text-field_focused" class into wrapping element', () => {
@@ -141,7 +142,7 @@ describe('2 Controlled component (AbstractField) testing', () => {
 
     it('2.2.2 calls onBlur callback', () => {
       input.simulate('blur');
-      expect(blur).toBeCalled();
+      expect(callback).toBeCalled();
     });
 
     it('2.2.3 checks that onBlur removed "text-field_focused" class from wrapping element', () => {
@@ -215,15 +216,92 @@ describe('2 Controlled component (AbstractField) testing', () => {
     });
 
     it('2.4.6 trims string on blur', () => {
-      let testNode = render(<AbstractField />);
-      let input = testNode.find('input').first();
+      let testNode = mount(<AbstractField />);
+      let input = testNode.find('input');
       let newVal = '   spaces';
       input.simulate('focus');
       input.node.value = newVal;
-      console.log(input.node.value);
-      input.simulate('change');
-      console.log(input.node.value);
+      input.simulate('change', {target: {value: newVal}});
+      input.simulate('blur');
       expect(input.node.value).toEqual(newVal.trim());
     });
+  });
+});
+
+describe('3 TextField Presentational testing', () => {
+  let callback;
+  beforeEach(() => {
+    callback = jest.fn();
+    jest.useFakeTimers();
+  });
+
+  it('3.1 checks that type of input is "text" by default', () => {
+    let node = mount(<TextFieldPresent />);
+    let type = node.props().type;
+    expect(type).toEqual('text');
+  });
+
+  it('3.2 checks that input type=password has an password reveal icon', () => {
+    let node = shallow(<TextFieldPresent type="password" />);
+    let reveal = node.find('EyePasswordIndicator').first();
+    expect(reveal).toBeDefined();
+  });
+
+  it('3.3 shows tooltip ', () => {
+    let node = mount(<TextFieldPresent />);
+    node.node.showTooltip(callback);
+    expect(callback).not.toBeCalled();
+    jest.runAllTimers();
+    expect(callback).toBeCalled();
+    expect(callback.mock.calls.length).toBe(1);
+  });
+
+  it('3.4 hides password and tooltip', () => {
+    let node = mount(<TextFieldPresent changeFieldType={callback} />);
+    let hideTooltip = jest.fn();
+    node.node.hidePasswordAndTooltip(hideTooltip);
+    expect(hideTooltip).not.toBeCalled();
+    jest.runAllTimers();
+    expect(hideTooltip).toBeCalled();
+    expect(callback).toBeCalledWith('password');
+  });
+
+  it('3.4.1 hides password and tooltip with force cleaning timer', () => {
+    let node = mount(<TextFieldPresent changeFieldType={callback} />);
+    node.instance().showTimer = true;
+    node.node.hidePasswordAndTooltip();
+    expect(callback).toBeCalled();
+  });
+
+  it('3.5 simulate mouseenter on reveal icon', () => {
+    let node = mount(<TextFieldPresent type="password" />);
+    let reveal = node.find('EyePasswordIndicator').first();
+    node.node.showTooltip = jest.fn(node.node.showTooltip);
+    reveal.simulate('mouseover');
+    expect(node.node.showTooltip).toBeCalled();
+  });
+
+  it('3.6 simulate touchstart on reveal icon', () => {
+    let node = mount(<TextFieldPresent type="password" />);
+    let reveal = node.find('EyePasswordIndicator').first();
+    node.node.showTooltip = jest.fn(node.node.showTooltip);
+    reveal.simulate('touchstart');
+    expect(node.node.showTooltip).toBeCalled();
+  });
+
+  it('3.7 simulate mouseleave on reveal icon', () => {
+    let node = mount(<TextFieldPresent type="password" />);
+    let reveal = node.find('EyePasswordIndicator').first();
+    node.node.hidePasswordAndTooltip = jest.fn(node.node.hidePasswordAndTooltip);
+    reveal.simulate('mouseleave');
+    expect(node.node.hidePasswordAndTooltip).toBeCalled();
+  });
+
+  it('3.8 simulate touchend on reveal icon', () => {
+    let node = mount(<TextFieldPresent type="password" />);
+    let reveal = node.find('EyePasswordIndicator').first();
+    node.node.hidePasswordAndTooltip = jest.fn(node.node.hidePasswordAndTooltip);
+    reveal.simulate('touchend');
+    expect(node.node.hidePasswordAndTooltip).toBeCalled();
   });
 });
