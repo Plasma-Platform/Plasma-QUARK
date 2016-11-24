16f859d9f9bf4530b5991049a5527126d6131ae4
Vadym Shymko <vadymshymko@gmail.com>
2016-11-24 18:36:41 +0200

    Added dropdown type 4

---
+++ src/components/dropdowns/DD4.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Dropdown from '../Dropdown.jsx';
+
+export default class DD4 extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.getValue = this.getValue.bind(this);
+  }
+
+  getValue () {
+    return this.dropdown.getValue();
+  }
+
+  render () {
+    return (
+      <Dropdown
+        {...this.props}
+        ref  = {ref => { this.dropdown = ref; }}
+        type = {4}
+      />
+    );
+  }
+}
