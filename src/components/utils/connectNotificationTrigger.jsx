9c68ae292f628867a8a77ac878410b965e3cca89
stevenreed <stevenreed@templatemonster.me>
2017-01-12 17:58:44 +0200

    fix bugs.PTR-245

--- src/components/utils/connectNotificationTrigger.jsx
+++ src/components/utils/connectNotificationTrigger.jsx
@@ -26,13 +26,15 @@ export default function connectNotificationTrigger (Component, props) {
       super(props, context);
       this.lastWidth = 0;
       this._originalCode = null;
-      if (typeof props.notification.button.action === 'string') {
-        switch (props.notification.button.action) {
-          case 'close':
-            props.notification.button.action = this.hideNotification;
-            break;
-          default:
-            break;
+      if (typeof props.notification.button !== 'undefined') {
+        if (typeof props.notification.button.action === 'string') {
+          switch (props.notification.button.action) {
+            case 'close':
+              props.notification.button.action = this.hideNotification;
+              break;
+            default:
+              break;
+          }
         }
       }
     }
