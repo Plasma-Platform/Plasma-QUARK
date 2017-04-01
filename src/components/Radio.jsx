0334c9245fd231ad1f455d0184d3917578bc5ccd
Vadym Shymko <vadymshymko@gmail.com>
2017-04-01 21:43:25 +0300

    Rebuil Radio comonent styles (fix bugs with checked && active state)

--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -3,31 +3,28 @@ import React from 'react';
 import './Radio.less';
 
 export default function Radio (props) {
-  const {className, label, ...radioProps} = props;
-  const addClassName = className ? ` ${className}` : '';
+  const {className, children, ...inputProps} = props;
+
+  const customClassName = className ? ` ${className}` : '';
 
   return (
     <label
-      className = {`tm-quark-radio${radioProps.disabled ? ' tm-quark-radio_disabled' : ''}${addClassName}`}
+      className = {`tm-quark-radio${customClassName}`}
       tabIndex  = "0"
     >
       <input
-        {...radioProps}
+        {...inputProps}
         className = "tm-quark-radio__input"
         type      = "radio"
       />
       <i className="tm-quark-radio__icon"></i>
-      <span className = "tm-quark-radio__label">{label}</span>
+      <span className="tm-quark-radio__label">
+        {children}
+      </span>
     </label>
   );
 }
 
 Radio.propTypes = {
-  className : React.PropTypes.string,
-  id        : React.PropTypes.string.isRequired,
-  name      : React.PropTypes.string.isRequired
-};
-
-Radio.defaultProps = {
-  tabIndex: 0
+  className: React.PropTypes.string
 };
